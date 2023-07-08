096051dfd9b91258636ae06ca4af8417c29c6d2c
Brunitux <51474180+SrBruno-JohnDark@users.noreply.github.com>
2023-07-07 22:52:44 -0300

    Update teste.sh
    update key

--- teste.sh
+++ teste.sh
@@ -1,5 +1,5 @@
 curl -L \
     -H "Accept: application/vnd.github+json" \
-    -H "Authorization: Bearer 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"\
+    -H "Authorization: Bearer sua_chave_aqui"\
     -H "X-GitHub-Api-Version: 2022-11-28" \
     https://api.github.com/users/$1
